728a1f2d670d33d85acb8ba300a4af5acc9cc80d
Kyle Sugrue <kyle@sugrue.ca>
2017-05-28 17:13:34 -0700

    added param to specify which interface/ip to bind on

--- lib/puppet/provider/swarm_cluster/ruby.rb
+++ lib/puppet/provider/swarm_cluster/ruby.rb
@@ -3,42 +3,37 @@ require 'resolv'
 
 Puppet::Type.type(:swarm_cluster).provide(:ruby) do
   desc "Support for Docker Swarm"
-  
+
   mk_resource_methods
 
   commands :swarm => "swarm"
   commands :ps => "ps"
-  commands :pidof => "pidof"   
-
-  def interface
-    hostname = Socket.gethostname
-    IPSocket.getaddress(hostname) 
-  end
+  commands :pidof => "pidof"
 
   def swarm_conf
-    cluster = resource[:cluster_type] 
+    cluster = resource[:cluster_type]
     backend = (resource[:backend])
     address = (resource[:address])
-    port = (resource[:port])  
-    advertise = (resource[:advertise])  
+    port = (resource[:port])
+    advertise = (resource[:advertise])
     path = (resource[:path])
-    case 
+    case
       when cluster.match(/create/)
         [['create']]
       when cluster.match(/join/)
-        [['--experimental', 'join', "--advertise=#{interface}:2375", "#{backend}://#{address}:#{port}/#{path}"]]
-      when cluster.match(/manage/)      
-        [['--experimental', 'manage', '-H', "tcp://#{interface}:2376", "#{backend}://#{address}:#{port}/#{path}"], ['--experimental', 'manage', '-H', ':4000', '--replication', '--advertise', "#{advertise}:4000", "#{backend}://#{address}:#{port}/#{path}"]] 
+        [['--experimental', 'join', "--advertise=#{bind}:2375", "#{backend}://#{address}:#{port}/#{path}"]]
+      when cluster.match(/manage/)
+        [['--experimental', 'manage', '-H', "tcp://#{bind}:2376", "#{backend}://#{address}:#{port}/#{path}"], ['--experimental', 'manage', '-H', ':4000', '--replication', '--advertise', "#{advertise}:4000", "#{backend}://#{address}:#{port}/#{path}"]]
       end
    end
 
    def exists?
       Puppet.info("checking if the swarm is running")
       args = ['-ef']
-      pid = ps *args  
+      pid = ps *args
       pid.match('swarm')
    end
- 
+
    def create
      Puppet.info("configuring the swarm cluster")
      swarm_conf.each do |conf|
